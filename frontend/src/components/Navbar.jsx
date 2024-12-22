a8f785ae09b7dc66c163dfb0edb4d4634fe0e356
Abhiiesante <142257623+Abhiiesante@users.noreply.github.com>
2024-12-22 04:35:34 +0000

    backend packages

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
                 <div className='group relative'>
                     <Link to='/login'><img className='w-6 cursor-pointer' src={assets.profile_icon} alt="" /></Link>
                     <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
-                        <div className='flex flex-col gap-2 w-36 py-3 bg-slate-100 text-gray-600 rounded-md shadow-md'>
+                        <div className='flex flex-col gap-2 w-36 px-4 py-3 bg-slate-100 text-gray-600 rounded-md shadow-md'>
                             <p className='cursor-pointer hover:text-black'>My Profile</p>
                             <p className='cursor-pointer hover:text-black'>Orders</p>
                             <p className='cursor-pointer hover:text-black'>Logout</p>
