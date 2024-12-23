92b0f0880790526aebea2bd17efd783f03113a0d
Abhiiesante <142257623+Abhiiesante@users.noreply.github.com>
2024-12-23 04:40:11 +0000

    login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -26,6 +26,7 @@ const Login = () => {
                 }
             } else {
                 const response = await axios.post("https://mytalorzone-backend.vercel.app", { email, password });
+                console.log(response.data);
                 if (response.data.success) {
                     setToken(response.data.token);
                     localStorage.setItem('token', response.data.token);
