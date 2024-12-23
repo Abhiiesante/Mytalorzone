738f90d50bd230af1715720e0670e89434fb8b16
Abhiiesante <142257623+Abhiiesante@users.noreply.github.com>
2024-12-23 08:35:40 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
         e.preventDefault();
         try {
             if (currentState === 'Sign Up') {
-                const response = await axios.post("https://mytalorzone-backend.vercel.app", { name, email, password });
+                const response = await axios.post("https://mytalorzone-backend.vercel.app/register", { name, email, password });
                 console.log(response.data);
                 if (response.data.success) {
                     setToken(response.data.token);
@@ -25,7 +25,7 @@ const Login = () => {
                     toast.error(response.data.message);
                 }
             } else {
-                const response = await axios.post("https://mytalorzone-backend.vercel.app", { email, password });
+                const response = await axios.post("https://mytalorzone-backend.vercel.app/login", { email, password });
                 if (response.data.success) {
                     setToken(response.data.token);
                     localStorage.setItem('token', response.data.token);
